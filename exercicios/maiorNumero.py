da53709bd06f14f927aae71b002a56bea4225677
Kamila Pantoja <kms.pantoja@gmail.com>
2023-01-02 10:07:12 -0400

    [feat]:exercicio maior numero

---
+++ exercicios/maiorNumero.py
@@ -0,0 +1,8 @@
+#Crie uma funcao que receba dois numeros e retorne o maior deles
+
+def bigger(number, other):
+    if other > number:
+        return other
+    return number
+
+print (bigger(3, 6))
